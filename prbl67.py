6bc7ff018521cd9078e788ddcac471279e76ec3c
pauloeduardodealmeidasantos <peas1211@gmail.com>
2020-03-01 06:07:24 +0100

    new file:   p067_triangle.txt new file:   prbl18.py new file:   prbl67.py

---
+++ prbl67.py
@@ -0,0 +1,27 @@
+#!/bin/python
+import numpy
+#p067_triangle.txt
+v = []
+aux=[]
+f = open("p067_triangle.txt", "r")
+for line in f:
+	w = line.split(" ")
+	aux=[]
+	for n in w:
+		aux.append(int(n))
+	v.append(aux)
+f.close()
+v = numpy.flip(v,0)
+def Maximo(a,b):
+	if(a > b):
+		return a
+	else:
+		return b
+s1 = 0
+s2 = 0
+for i in range(1,len(v)):
+	for j in range(0, len(v[i])):
+		s1 = v[i][j] + v[i-1][j]
+		s2 = v[i][j] + v[i-1][j+1]
+		v[i][j] = Maximo(s1,s2)
+print(v[-1])
