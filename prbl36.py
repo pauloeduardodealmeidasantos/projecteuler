af1d31b7bbad04765173d29b31bb009980fab64c
pauloeduardodealmeidasantos <peas1211@gmail.com>
2020-03-03 01:31:07 +0100

    new file:   prbl34.py new file:   prbl35.py new file:   prbl36.py new file:   prbl37.py new file:   prbl39.py

---
+++ prbl36.py
@@ -0,0 +1,14 @@
+#!/bin/python
+import numpy
+def isPalindroma(str):
+	return str == str[::-1]
+def GetBinario(num):
+	ret = numpy.binary_repr(num)
+	if(ret[0]=='0'):
+		ṕrint(ret)
+	return ret
+v1 = []
+for i in range(1,1000000):
+	if(isPalindroma(str(i)) and isPalindroma(GetBinario(i))):
+		v1.append(i)
+print(v1,len(v1), sum(v1))
