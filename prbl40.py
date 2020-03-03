2e97b414d2c807969218715c40d3276a5804990e
pauloeduardodealmeidasantos <peas1211@gmail.com>
2020-03-03 08:43:20 +0100

    commit 41

---
+++ prbl40.py
@@ -0,0 +1,11 @@
+#!/bin/python
+tamanho = 1000010
+string = ''
+for i in range(tamanho):
+	string = string + str(i)
+for i in range(7):
+	print(10**i,string[10**i])
+	
+	
+	
+	
---
+++ prbl41.py
@@ -0,0 +1,20 @@
+#!/bin/python
+import sympy
+import itertools
+def isPanDigital(w,n):
+	v = ['1', '2', '3', '4', '5', '6', '7', '8', '9'][:n]
+	s = ''+w
+	s = sorted(s)
+	return s == v
+v = [1,2,3,4,5,6,7,8,9]
+obj=list()
+lst = list()
+for num in v:
+	lst = lst + list(itertools.permutations(v[:num]))
+for item in lst:
+	if(isPanDigital(''.join(map(str, item)),len(item)) and sympy.isprime(int(''.join(map(str, item))))):
+		obj.append(int(''.join(map(str, item))))
+for i in obj:
+	print(i)
+print('########')
+print(max(obj))
