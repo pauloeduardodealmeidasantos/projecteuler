e723e239d53f0879e2a3b4470ef310e5c122a5a3
pauloeduardodealmeidasantos <peas1211@gmail.com>
2020-03-05 18:53:45 +0100

    modified:   prbl47.py new file:   prbl48.py new file:   prbl49.py

--- prbl47.py
+++ prbl47.py
@@ -1,20 +1,30 @@
 #!/bin/python
+from sympy import *
+import sympy
 import sys
-import primefac
-def GetNumberDistinctPrimes(num):
-	return len(set(primefac.primefac(num)))
-RAZAO = 2
-continueLooping = True
-count = 10
-res = list()
-while(continueLooping):
-	lst = list()
-	for num in range(count,RAZAO):
-		d = GetNumberDistinctPrimes(num)
-		print(count,num,d)
-		if(d in lst):
-			continueLooping = False
-			break
-			print(consecs)
-		lst.append(d)
-	count +=1	
+def numberDistinctPrimes(num,lst):
+	count = 0
+	ret = list()
+	for elem in lst:
+		if(num % elem == 0):
+			count += 1
+			ret.append(elem)
+			if(count > 4):
+				return [ret,count]
+	return [ret,count]
+lst = list(sieve.primerange(1,5))
+i = 2
+j = 3
+k = 4
+w = 5
+while(true):
+	if(lst[-1] < i+4):
+		lst.append(nextprime(lst[-1], ith=1))
+	if(numberDistinctPrimes(i,lst)[1] == 4 and numberDistinctPrimes(j,lst)[1] == 4 and numberDistinctPrimes(k,lst)[1] == 4 and numberDistinctPrimes(w,lst)[1] == 4):
+		print(i,j, k,w)
+		sys.exit()
+	i+=1
+	j+=1
+	k+=1
+	w+=1
+	
---
+++ prbl49.py
@@ -0,0 +1,20 @@
+#!/bin/python
+import sympy
+import sys
+import itertools
+from sympy import *
+def IsPermut(n,m):
+	s1 = list(set(str(n)))
+	s2 = list(set(str(m)))
+	return s1==s2
+lst = list(sieve.primerange(1000,9999))
+perm = list()
+#a lista de primos já está ordenada...
+for i in range(0,len(lst)):
+	for j in range(i+1,len(lst)):
+		for k in range(j+1,len(lst)):
+			n1 = lst[i]
+			n2 = lst[j]
+			n3 = lst[k]
+			if(n3 - n2 == 3330 and n3 - n2 == n2 - n1 and IsPermut(n1,n2) and IsPermut(n2,n3)):
+				print(n1,n2,n3)
