572d06395f8bcb000bef5d4a48504f410e44d090
pauloeduardodealmeidasantos <peas1211@gmail.com>
2020-03-08 02:51:31 +0100

    modified:   prbl50.py

--- prbl50.py
+++ prbl50.py
@@ -2,18 +2,33 @@
 from sympy import *
 import sys
 import sympy
-lst = list(sieve.primerange(2,10**3))
-init = len(lst)
-for i in range(init,3,-1):
-	for j in range(1,len(lst)):
-		k = lst[-1*j]
-		c = 0
-		s = 0
-		while(s<=k):
-			sublist = lst[c:]
-			sublist = sublist[:i]
-			s = sum(sublist)
+import math
+def MaiorSubLista(sublista,n):
+	for j in range(0,len(sublista)):
+		pList = sublista[j:]
+		s = sum(pList)
+		c = maior
+		while(len(pList) > maior and s > n):
+			pList = pList[:-1]
+			s = sum(pList)
 			c+=1
-			if(s==k):
-				print(sublist,c,s,k)
-				sys.exit()
+		if(s==n):
+			return len(pList)
+	return -1
+pot = 2
+maior = -1
+lstprimerange = list(sieve.primerange(2,10**pot))
+for position in range(len(lstprimerange)-1,3,-1):
+	print('##########################################')
+	print(lstprimerange[position])
+	print('##########################################')
+	limit = int((lstprimerange[position]+1)/2)
+	sublist = list(filter(lambda x: x < limit, lstprimerange))
+	while(sublist[-1]+sublist[-2] > lstprimerange[position] and sum(sublist) <= lstprimerange[position]):
+		limit = int((sublist[-1]+1)/2)
+		sublist = list(filter(lambda x: x < limit, sublist))
+	ret = MaiorSubLista(sublist,lstprimerange[position])
+	if(ret > maior):
+		print(ret,lstprimerange[position])
+		maior = ret
+print('maior::'+str(maior))
