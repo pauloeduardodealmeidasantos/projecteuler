7c24d5227e9f05d141bebdc5f3c897e5097253b1
Paulo Almeida <pauloeduardodealmeidasantos@gmail.com>
2020-07-20 12:07:37 -0300

    new file:   prbl123.py new file:   prbl214.py new file:   prbl76.py new file:   prbl77.py

---
+++ prbl76.py
@@ -0,0 +1,12 @@
+#!/bin/python
+lim=100
+#sizes=[1,2,5,10,20,50,100,200]
+sizes=range(1,lim)
+ways=[]
+for i in range(0,lim+2):
+	ways.append(0)
+ways[0]=1
+for i in range(0,len(sizes)):
+	for j in range(sizes[i],lim+1):
+		ways[j] += ways[j-sizes[i]]
+print(ways[lim])
